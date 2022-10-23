b36092cdd2a645d17286740d8fd1b8042275daba
Leonid Jakimovski <ljakimovski211@yahoo.com>
2022-10-23 13:24:38 +0200

    init

---
+++ src/app/layout/main-layout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.scss']
+})
+export class MainLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(HttpClientModule)]
+}).catch(err => console.error(err));
